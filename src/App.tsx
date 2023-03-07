890245d1a6fdae18d980343010d0663b3f4e6c89
facubozzi <facundo.bo@hotmail.com>
2023-03-06 23:06:24 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Button, Grid, GridItem } from '@chakra-ui/react';
 
 function App() {
+  const [nfts, setNfts] = useState([]);
+  const ethAddress = '0x7292a63772E7939eF6cb3B94f997Be860Fb0754e'; // Replace with the Ethereum address you want to get NFTs for
+
+  const getNfts = async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.opensea.io/api/v1/assets',
+      params: {
+        owner: '0x7292a63772E7939eF6cb3B94f997Be860Fb0754e',
+        order_direction: 'desc',
+      },
+      headers: {accept: 'application/json'}
+    };
+    
+    axios
+      .request(options)
+      .then(function (response) {
+        setNfts(response.data.assets);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getNfts().then(() => console.log("los nfts", nfts))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid>
+      <h1>NFTs owned by {ethAddress}</h1>
+      {nfts.map((nft: any) => (
+        <GridItem key={nft.id}>
+          <img src={nft.image_thumbnail_url} alt={nft.name} />
+          <h2>{nft.name}</h2>
+          <h2>{nft.description}</h2>
+        </GridItem>
+      ))}
+    </Grid>
   );
 }
 
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export interface NFT {
+    name: string;
+    description: string;
+    image_thumbnail_url: string;
+    id: number;
+}
